e9478540f8c155fb1c6836f3618baca37116d39e
Nills <nills.van.limbeek@live.nl>
2020-06-13 21:14:28 +0200

    server

---
+++ src/lib/server/server.ts
@@ -0,0 +1,9 @@
+// interface Props<TData> {
+//     data: TData;
+// }
+
+// export const server = (url: string) => {
+//     fetch(url)
+//         .then((res) => res.json())
+//         .then((data) => data);
+// };
