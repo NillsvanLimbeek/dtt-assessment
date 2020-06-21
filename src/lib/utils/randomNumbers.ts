838cd86f8e2e17938f17ecdbff05ae727889490f
Nills <nills.van.limbeek@live.nl>
2020-06-21 22:12:01 +0200

    ...

--- src/lib/utils/randomNumbers.ts
+++ src/lib/utils/randomNumbers.ts
@@ -2,9 +2,9 @@ export function randomNumbers(count: number, max: number) {
     // set only accepts unique values
     const numbers = new Set<number>();
 
-    for (let i = 1; i <= count + 2; i++) {
+    for (let i = 1; i <= count; i++) {
         numbers.add(Math.ceil(Math.random() * max));
     }
 
-    return [...numbers].slice(0, count);
+    return [...numbers];
 }
